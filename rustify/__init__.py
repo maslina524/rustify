8b785e5a7e5bff4cc6ad5480db43d7e891649be0
maslina524 <luk.kirill2804@gmail.com>
2026-05-09 22:00:30 +0300

    added prelude

--- rustify/__init__.py
+++ rustify/__init__.py
@@ -3,11 +3,4 @@
 __version__ = "0.1.0"
 __author__ = "maslina524"
 
-from .core.result import Result, Ok, Err
-from .core.option import Option, Some
-from .test import test, cfg_tests, assert_eq, assert_ne
-from .std.dbg import dbg
-from .cfg import cfg, all, any
-from .lib import start, deprecated
-
 from .error_handler import rust_error_handler
---
+++ rustify/prelude.py
@@ -0,0 +1,8 @@
+from .core.result import Result, Ok, Err
+from .core.option import Option, Some
+
+from .std.dbg import dbg
+
+from .cfg import cfg
+from .lib import deprecated, start
+from .test import test, cfg_tests
